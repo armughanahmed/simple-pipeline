837b83b3b126f670883750fd29068e1b35eb4e4a
armughanahmed <armughan.ahmed@yahoo.com>
2021-03-24 03:59:00 +0500

    jenkinsfile fixed

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo 'building the docker image...'
     withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t $USER/demo-app:jma-2.0 ."
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push $USER/demo-app:jma-2.0"
+        docker.withRegistry('https://registry.hub.docker.com', 'dockerhub') {
+            docker.build("$USER/demo-app:jma-2.0", '.').push()
+        }
     }
 }
 
